f394ee271c57195d689c96e9cce253978cff32aa
LatSon2023 <2023newbie>
2023-02-27 10:10:07 +0700

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
+const allItems = [
+    { id: 'beaming', value: '😁', isSmile: true},
+    { id: 'grinning', value: '😀', isSmile: true},
+    { id: 'squinting', value: '😄', isSmile: true},
+    { id: 'winking', value: '😉', isSmile: true},
+    { id: 'sad', value: '😥', isSmile: false},
+]
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    const [items, setItems] = useState(allItems)
+
+    const handleLogin = () => {
+        setIsLoggedIn(true)
+    }
+    const handleLogout = () => {
+        setIsLoggedIn(false)
+    }
+    const removeItem = (itemId) => {
+        setItems(items.filter(item => item.id !== itemId))
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Smileys</h1>
+        <div className='loginControl'>
+            {isLoggedIn ? (
+            <button onClick={handleLogout}>Logout</button>
+            ) : (
+            <button onClick={handleLogin}>Login</button>
+            )}
+        </div>
+        <div>
+            {isLoggedIn && (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id} onClick={() => removeItem(item.id)}>
+                            <label htmlFor={`${item.id}-input`}>{item.value} {item.id}</label>
+                            <input id={`${item.id}-input`} type="text"  defaultValue={item.id}/>
+                            <button>Remove</button>
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
